9a2de093676074b6628dd11970442e3d40359aaa
nimishabora <65830448+nimishabora@users.noreply.github.com>
2024-06-06 22:27:19 -0500

    Add files via upload

---
+++ src/Card.js
@@ -0,0 +1,30 @@
+export default () => (
+    <div className="birthdayCard">
+      <div className="cardFront">
+        <h3 className="happy">HAPPY BIRTHDAY!</h3>
+        <div className="balloons">
+          <div className="balloonOne" />
+          <div className="balloonTwo" />
+          <div className="balloonThree" />
+          <div className="balloonFour" />
+        </div>
+      </div>
+      <div className="cardInside">
+        <h3 className="back">
+          <a
+            id="button"
+            href="https://www.canva.com/design/DAGHTtyVRO4/f4eo_XaqXhPW-c3iV0m84w/view?utm_content=DAGHTtyVRO4&utm_campaign=designshare&utm_medium=link&utm_source=editor"
+          >
+            HAPPY BIRTHDAY DADDY!
+          </a>
+        </h3>
+        <p>Dear Daddy,</p>
+        <p>
+          Happy 50th Birthday!! We hope your day is filled with lots of love and
+          laughter! Here's a collection of beautiful memories with you.
+        </p>
+        <p className="name">Love, Nimisha & Taj</p>
+      </div>
+    </div>
+  );
+  
